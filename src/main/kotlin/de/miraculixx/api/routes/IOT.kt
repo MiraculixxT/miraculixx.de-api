a2e95e423fdad57c13c32a9d597268a1ddae3da9
Miraculixx <miraculixxyt@gmail.com>
2025-01-07 20:48:39 +0100

    Add getAll and getSubmitted endpoints

--- src/main/kotlin/de/miraculixx/api/routes/IOT.kt
+++ src/main/kotlin/de/miraculixx/api/routes/IOT.kt
@@ -23,31 +23,59 @@ fun Routing.routingIOT() {
                 call.respond(character)
             }
 
-            get("audio/{character}/{id}") {
+            get("list") {
                 val token = call.request.header("Authorization") ?: return@get respondUnauthorized()
                 val session = Authentication.getSession(token) ?: return@get respondUnauthorized()
-                val characterName = call.parameters["character"] ?: return@get call.respond(HttpStatusCode.BadRequest, "Missing character header")
-                val audioID = call.parameters["id"] ?: return@get call.respond(HttpStatusCode.BadRequest, "Missing audio id")
-                val audio = IOT.getAudio(characterName, audioID, session.id) ?: return@get call.respond(HttpStatusCode.NotFound, "Audio not found")
+                val characters = IOT.getAllCharacters(session.id)
 
-                call.respond(audio)
+                IOT.logger.info("Requested character list")
+                call.respond(characters)
             }
 
-            post("audio/{character}/{id}") {
-                val token = call.request.header("Authorization") ?: return@post respondUnauthorized()
-                val session = Authentication.getSession(token) ?: return@post respondUnauthorized()
-                val characterName = call.parameters["character"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Missing character header")
-                val audioID = call.parameters["id"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Missing audio id")
-                if (IOT.getAudio(characterName, audioID, session.id) == null) return@post call.respond(HttpStatusCode.NotFound, "Audio not found")
+            route("audio") {
+                get("{character}/{id}") {
+                    val token = call.request.header("Authorization") ?: return@get respondUnauthorized()
+                    val session = Authentication.getSession(token) ?: return@get respondUnauthorized()
+                    val characterName = call.parameters["character"] ?: return@get call.respond(HttpStatusCode.BadRequest, "Missing character header")
+                    val audioID = call.parameters["id"] ?: return@get call.respond(HttpStatusCode.BadRequest, "Missing audio id")
+                    val audio = IOT.getAudio(characterName, audioID, session.id) ?: return@get call.respond(HttpStatusCode.NotFound, "Audio not found")
 
-                // Receive audio data
-                val audio = call.receiveStream().readBytes()
-                val targetFile = File("data/iot/voice/submits/$characterName/$audioID.mp3")
-                if (!targetFile.exists()) targetFile.parentFile.mkdirs()
-                targetFile.writeBytes(audio)
+                    call.respond(audio)
+                }
 
-                IOT.logger.info("Received audio submission for $characterName/$audioID")
-                call.respond(HttpStatusCode.OK)
+                post("{character}/{id}") {
+                    val token = call.request.header("Authorization") ?: return@post respondUnauthorized()
+                    val session = Authentication.getSession(token) ?: return@post respondUnauthorized()
+                    val characterName = call.parameters["character"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Missing character header")
+                    val audioID = call.parameters["id"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Missing audio id")
+                    if (IOT.getAudio(characterName, audioID, session.id) == null) return@post call.respond(HttpStatusCode.NotFound, "Audio not found")
+
+                    // Check file size
+                    val contentLength = call.request.header(HttpHeaders.ContentLength)?.toLongOrNull()
+                    if (contentLength == null || contentLength > 10 * 1024 * 1024) {
+                        return@post call.respond(HttpStatusCode.PayloadTooLarge, "File size exceeds the 10MB limit")
+                    }
+
+                    // Receive audio data
+                    val stream = call.receiveStream()
+                    val audio = stream.readBytes().also { stream.close() }
+                    val targetFile = File("data/iot/voice/submits/$characterName/$audioID.mp3")
+                    if (!targetFile.exists()) targetFile.parentFile.mkdirs()
+                    targetFile.writeBytes(audio)
+
+                    IOT.logger.info("Received audio submission for $characterName/$audioID")
+                    call.respond(HttpStatusCode.OK)
+                }
+
+                get("submits/{character}/{id}") {
+                    val token = call.request.header("Authorization") ?: return@get respondUnauthorized()
+                    val session = Authentication.getSession(token) ?: return@get respondUnauthorized()
+                    val characterName = call.parameters["character"] ?: return@get call.respond(HttpStatusCode.BadRequest, "Missing character header")
+                    val audioID = call.parameters["id"] ?: return@get call.respond(HttpStatusCode.BadRequest, "Missing audio id")
+                    val audio = IOT.getSubmitAudio(characterName, audioID, session.id) ?: return@get call.respond(HttpStatusCode.NotFound, "Audio not found")
+
+                    call.respond(audio)
+                }
             }
         }
     }
